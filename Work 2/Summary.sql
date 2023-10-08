2d6518013b56343d9eb933d43977790d2e2ad474
Mark Nikulov <87237085+ElderScroS@users.noreply.github.com>
2023-10-08 21:13:41 +0400

    Update Summary.sql

--- Work 2/Summary.sql
+++ Work 2/Summary.sql
@@ -1,6 +1,5 @@
 ===============================================<<<SelectQuery>>>==============================================
 
---1. Вывести содержимое таблицы 
 SELECT [Id]
       ,[Name]
       ,[Surname]
@@ -14,38 +13,29 @@ SELECT [Id]
       ,[EndPositionDate]
   FROM Summary.dbo.Employee
 
---2. Вывести фамилии и zarplati всех rabocix.
 SELECT [Surname], [Salary]
  FROM Employee
 
---3. Вывести все familii без повторений.
 SELECT DISTINCT Surname
  FROM Employee
 
---4. Vivesti vse departamenti i pozitii
 SELECT [Departament], [Position]
  FROM Employee
-
---5. Vivesti vse departamenti bez povtoreniy
+	
 SELECT DISTINCT Departament
  FROM Employee
 
---6. Vivesti vse imena s favimiliyami v odnom stolbce i departament s poziciyeye vo vtorom stolbce
 SELECT [Name] + ' ' + [Surname] as FullName, [Departament] + ' ' + [Position] as WorkPlace
  FROM Employee
 
---7. vivesti rebotnikov kotoriye polucili bonus
 SELECT [Name]
  FROM Employee WHERE (Bonus > 0)
 
---8.Vivesti vsex rabotnikov cya zarplata i bonusi v summe previwayut 1500 azn
 SELECT [Name], [Salary] + [Bonus] as General
  FROM Employee WHERE (Salary + Bonus > 1500)
 
---9.Vivesti vsex rabotnikov cya polovina zarplati previwaet bonus bolshe cem v 3 raza
 SELECT [Name]
  FROM Employee WHERE ((Salary / 2) / Bonus > 3)
-
 
 ==================================================<<<TableQuery>>>===============================================
 
@@ -75,6 +65,3 @@ CREATE TABLE Employee
 4	Huseyn	Aliyev	1990-05-16	False	16000.0000	1200.0000	C++	Suggests	2022-01-01	2027-01-01
 5	Ayten	AY-10	1999-08-08	True	34000.0000	2500.0000	Assembler	InnerVis	2024-01-01	2035-01-01
 NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL	NULL
-
-
-/// Вы скопируйте и вставьте в edit table
